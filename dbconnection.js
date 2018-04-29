0c59777b68301320084199edfbc19b6d6fcf880b
Ceiline <ceiline@icloud.com>
2018-04-28 20:45:18 -0700

    database hosted online

--- dbconnection.js
+++ dbconnection.js
@@ -1,11 +1,10 @@
 var mysql=require('mysql');
 
 var connection=mysql.createConnection({
-   host:'localhost',
+   host:'task.cyyb0zpneg8q.us-east-2.rds.amazonaws.com',
    user:'root',
-   password:'root',
-   database:'sys',
+   password:'roottask',
+   database:'task',
    port:'3306',
 });
-
 module.exports=connection;
