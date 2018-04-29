d9f1415afd508253ccf8f8a00ffa9a963d5da7a2
Ceiline <ceiline@icloud.com>
2018-04-28 22:02:31 -0700

    createpool

--- dbconnection.js
+++ dbconnection.js
@@ -1,18 +1,13 @@
 var mysql=require('mysql');
 
-var connection=mysql.createConnection({
+var connection=mysql.createPool({
+
    host:'task.cyyb0zpneg8q.us-east-2.rds.amazonaws.com',
    user:'root',
    password:'roottask',
    database:'task',
    port:'3306',
-});
-connection.connect(function(err) {
-  if(err){
-  console.error('error connecting: ' + err.stack);
-  return;
-  }
-  console.log('connected as id ' + connection.threadId);
+
 });
 
 module.exports=connection;
